133fc76e8b9d2b3faeb7191be0ae2de604f890e2
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-02-23 17:10:44 +0100

    Minor.

--- friendship/managers.py
+++ friendship/managers.py
@@ -1,20 +1,26 @@
 from django.db import models
+from .exceptions import FriendshipRequestExists
+from django.db.models import Q
 
 class FriendshipRequestManager(models.Manager):
     def send_request(self, by, to):
         request = self.filter(by=by, to=to)
-        if request:
-            return None
+        if request.exists():
+            raise FriendshipRequestExists
         request = self.create(by=by, to=to)
-        return request
 
 class FriendshipManager(models.Manager):
-    def is_friend(self, us1, us2):
-        try:
-            self.get(by=us1, to=us2)
-        except self.model.DoesNotExist:
-            return False
-        return True
+    def are_friends(self, us1, us2):
+        qs = self.filter(
+            Q(by=us1.email) & Q(to=us2.email)
+            |
+            Q(by=us2.email) & Q(to=us1.email)
+        )
+        return qs.exists()
 
     def add_friend(self, us1, us2):
-        pass
+        if are_friends(us1, us2):
+            raise FriendshipExists
+
+        self.create(by=us1, to=us2)
+        self.create(by=us2, to=us1)
