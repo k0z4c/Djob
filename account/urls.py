a58123f463733df849bd0b0b22ddd61d692327c6
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-03-08 18:52:06 +0100

    Refactoring

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,16 @@ from django.conf.urls import url, include
 from . import views
 
 some_urls = [
-    url('^$', views.ProfileDetailView.as_view(), name='profile_detail'),
-    url('^edit/$', views.EditAccountView.as_view(), name='profile_edit'),
+    url(
+        '^$',
+        views.ProfileDetailView.as_view(),
+        name='profile_detail'
+        ),
+    url(
+        '^edit/$',
+        views.EditAccountView.as_view(),
+        name='profile_edit'
+        ),
 ]
 
 urlpatterns = [
@@ -11,9 +19,9 @@ urlpatterns = [
         '^$',
         views.index,
         name='index',
-    ),
+        ),
     url(
         '^(?P<email>(\w)+@(\w)+\.(\w)+)/',
         include(some_urls),
-    )
+        ),
 ]
