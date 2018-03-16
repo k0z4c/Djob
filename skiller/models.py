1e555d61685c307521d3d8180972429d1d2e13d9
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-03-16 18:59:10 +0100

    Fix skill model with ForeignKey

--- skiller/models.py
+++ skiller/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings 
+from .managers import SkillManager
 
 class Skill(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     serial = models.AutoField(primary_key=True)
-    codename = models.CharField(max_length=50, help='codename for the skill')
+    codename = models.CharField(max_length=50, unique=True, help_text='codename for the skill')
 
-    confirmations = models.IntgerField(default=0)
+    confirmations = models.IntegerField(default=0)
     manager = SkillManager()
 
     def __str__(self):
