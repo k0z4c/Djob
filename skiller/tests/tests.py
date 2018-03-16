3c3e22e4f9dbb19bae1c86d99a78fb4146a1791e
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-03-16 19:05:56 +0100

    Add dumb test on decorate_name for skiller manager

--- skiller/tests/tests.py
+++ skiller/tests/tests.py
@@ -11,6 +11,13 @@ from authentication.tests.factories import UserFactory
 from django.db import IntegrityError
 class SkillTestCase(TestCase):
 
+    def test_decorate_name(self):
+        bad_codename = "sk i    "
+        user = UserFactory()
+
+        skill = Skill.manager.insert_skill(user=user, name=bad_codename)
+        self.assertEqual(skill.codename, 'sk_i')
+
     def test_skill_exists_error(self):
         user = UserFactory()
         codename = 'this is a codename'
