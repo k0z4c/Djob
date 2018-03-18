380945f8cd0397bbc343a3e2add235f396ae7507
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-03-18 22:23:35 +0100

    Change confirmations to ForeignKey

--- skiller/models.py
+++ skiller/models.py
@@ -4,11 +4,11 @@ from .managers import SkillManager
 
 class Skill(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    confirmations = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     serial = models.AutoField(primary_key=True)
     codename = models.CharField(max_length=50, unique=True, help_text='codename for the skill')
 
-    confirmations = models.IntegerField(default=0)
     manager = SkillManager()
 
     def __str__(self):
