f02b337a56037e5da4b4d4d9abb4f4a2b69a954c
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-04-10 00:04:50 +0200

    Refactoring test add skill

--- skiller/tests/tests.py
+++ skiller/tests/tests.py
@@ -4,27 +4,23 @@ from authentication.tests.factories import UserFactory
 from django.db import IntegrityError
 from ..exceptions import DuplicatedSkill
 from ..models import Skill, SkillData 
-from .factories import SkillFactory 
+from .factories import SkillFactory, SkillDataFactory
 from django.test import Client
 from django.core.urlresolvers import reverse 
 
 
 class SkillModelTestCase(TestCase):
     # models integrity checks 
-    def test_double_skill_association_fail(self):
-        skill = SkillFactory(data__codename='hey')
-        with self.assertRaises(IntegrityError):
-            SkillFactory(user=skill.user, data___codename='hey')
 
-    def test_manager_add_skill(self):
-        skill_association = Skill.objects.add_skill(user=UserFactory(), name='hey man')
+    def test_user_add_skill_success(self):
+        user = UserFactory()
+        data = SkillDataFactory(codename='testing')
 
-        # add skill that not exists 
-        skill_data = SkillData.objects.get(_codename='hey_man')
-        self.assertEqual(skill_data._codename, 'hey_man')
+        Skill.objects.create(user=user, data=data)
+        self.assertTrue(Skill.objects.get(user=user, data=data))
+        self.assertTrue(user.skill_set.get(user=user, data=data))
+        self.assertTrue(data.skill_set.get(user=user, data=data))
 
-        with self.assertRaises(DuplicatedSkill):
-            Skill.objects.add_skill(user=skill_association.user, name='hey man')
 
 
 class SkillViewTestCase(TestCase):
@@ -68,20 +64,4 @@ class SkillViewTestCase(TestCase):
         self.assertEqual(len(queue), 1)
         self.assertEqual(queue[0].level, SUCCESS)
 
-    def test_post_add_skill_message_error_double_skill(self):
-        response = self.client.post(
-                reverse('skill:add_skill'),
-                {'_codename': 'skill'},
-                follow=True,
-                )
-
-        response = self.client.post(
-                reverse('skill:add_skill'),
-                {'_codename': 'skill'},
-                follow=True,
-                )
-
-        from django.contrib.messages import ERROR
-        queue = list(response.context['messages']) 
-        self.assertEqual(len(queue), 1)
-        self.assertEqual(queue[0].level, ERROR)       
+       
