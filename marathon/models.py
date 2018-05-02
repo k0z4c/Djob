80af9425fb4dcd7c00fe49722ff1be8ddef5bb0f
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-05-02 23:13:36 +0200

    Add models

--- marathon/models.py
+++ marathon/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+
+class RequestManager(models.Manager):
+  pass
 
 # Create your models here.
+class Request(models.Model):
+  by = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    related_name='marathon_sended')
+  
+  to = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    related_name='marathon_received')
+
+  class Meta:
+    unique_together = (('by', 'to'),)
+
+  label = models.CharField(max_length=30, default='')
+  message = models.CharField(max_length=70, default='')
+  is_accepted = models.BooleanField(default=False)
+  is_rejected = models.BooleanField(default=False)
+
+  def accept(self):
+    self.is_accepted = True
+    self.save()
+
+  def reject(self):
+    self.is_rejected = False
+    self.save()
