9ca8b1313cfdc8aaa6f38864736c00ef8566d9ce
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-05-26 16:42:24 +0200

    Add Invite capability for projectpager.

--- projectpager/forms.py
+++ projectpager/forms.py
@@ -6,11 +6,47 @@ from crispy_forms.layout import (
 )
 
 from .models import ProjectPage
-#  real model form -> create invite; createview? 
-class InviteForm(forms.ModelForm):
-  pass
 
-# maybe update view? 
+from marathon.models import SocialRequest
+
+class InviteForm(forms.Form):
+  group = forms.ModelChoiceField(queryset=None)
+  to = forms.ModelMultipleChoiceField(queryset=None, required=False)
+
+  def __init__(self, profile, *args, **kwargs):
+    super(InviteForm, self).__init__(*args, **kwargs)
+    self.helper = FormHelper()
+    self.helper.add_input(Submit('submit', 'Submit'))
+
+    self.profile = profile
+    self.fields['group'].queryset = profile.projectpages.all()
+    self.fields['to'].queryset = profile.contacts.all()
+
+  def save(self, commit=False):
+    if self.errors:
+        raise ValueError(
+            "The %s could not be %s because the data didn't validate." % (
+                self.instance._meta.object_name,
+                'created' if self.instance._state.adding else 'changed',
+            )
+        )
+    invites = []
+    qs = self.cleaned_data['to']
+    for friendship in qs:
+      invites.append(
+        SocialRequest.objects.send_request(
+          label='invite_request',
+          tile='{} invites you to join {} group.'.format(
+            self.profile.user.email,
+            self.cleaned_data['group'].name
+            ),
+          by=self.profile,
+          to=friendship.to,
+          data={}
+        )
+      )
+    return invites
+
 class UpdateProjectPageForm(forms.ModelForm):
   pass
 
@@ -30,9 +66,9 @@ class CreateProjectPageForm(forms.ModelForm):
       we have to save the instance before
       set manytomanyfield (users)
     '''
-    self.instance.owner = user
+    self.instance.owner = user.profile
     project_page = super(CreateProjectPageForm, self).save(*args, **kwargs)
     if not kwargs.get('commit', ''):
-      project_page.users.add(user)
+      project_page.profiles.add(user.profile)
       project_page = super(CreateProjectPageForm, self).save(*args, **kwargs)
     return project_page
--- projectpager/models.py
+++ projectpager/models.py
@@ -1,23 +1,5 @@
 from django.db import models
-from django.conf import settings 
-
 from account.models import Profile
-# here we have to instantiated Project page before create an Invite request
-# Create your models here.
-class InviteRequest(models.Model):
-  by = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    related_name='invites_sended')
-  to = models.ForeignKey(
-    settings.AUTH_USER_MODEL,
-    related_name='invites_received')
-
-  project_page = models.OneToOneField('ProjectPage')
-
-  class Meta:
-    unique_together = (('by', 'to'),)
-
-  date = models.DateTimeField(auto_now_add=True)
 
 class ProjectPage(models.Model):
   profiles = models.ManyToManyField(
@@ -31,5 +13,9 @@ class ProjectPage(models.Model):
   name = models.CharField(max_length=50)
   description = models.TextField()
 
+  class Meta:
+    permissions = (
+      ('can_post_discussions', 'Can post new discussions to page.'),
+      )
   def __str__(self):
     return self.name
