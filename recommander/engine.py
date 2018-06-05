a3e516491e5b5131f5a8d4911196ef1caa1a109b
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-05 17:33:10 +0200

    Fix SuggestView and predictionEngine

--- recommander/engine.py
+++ recommander/engine.py
@@ -1,21 +1,6 @@
 
 class PredictionEngine(object):
-  # contacts e projectpages sono modelli intermedi 
-  # attrs = [('contacts', 'to'), 'projectpages', ('skill_set', 'data')]
-  #                       -> attribute to access for intermediate model's instances 
-  #  
-  #  in kwargs we put this: list =
-   #  if tuple:
-  # ( getattr(f, 'to') for f in contacts.all())
-  # ( getattr(s, 'data') for s in skill_set.all())
-  # ( getattr(p, 'data') for p in project_pages.all())
-
-  # ( f.to for f in contacts.all())
-  # [(list of profiles friends), (list of projectpages instances), (list of ), ()]
   def __init__(self, x, population, attrs, sample_misure=10):
-    '''
-      inputs are profiles or qs?
-    '''
     self.x = x
     self.attrs = attrs
     self.population = population
@@ -25,7 +10,7 @@ class PredictionEngine(object):
 
   def _sample(self, population, sample_misure):
     import random
-    if len(population) < sample_misure: 
+    if len(list(population)) < sample_misure:
       return population
     return random.sample(set(population), sample_misure)
 
@@ -37,23 +22,14 @@ class PredictionEngine(object):
     return ( r[0] for r in results_list )
   
   def fitness_score(self, x, y):
-      '''
-        returns the fitness score between x and y profiles on attrs.
-        filter may be a callable for filtering on these attrs (skill_set data)
-      '''
       score = 0
       for attr in self.attrs:
         if isinstance(attr, tuple):
-          # we substitute the intermediate records with a select on a particular attribute (e.g. friendhip's intances)
-          # this may be a good lambda girl
           x_attr_set = set(( getattr(f, attr[1]) for f in getattr(x, attr[0]).all() ))
           y_attr_set = set(( getattr(f, attr[1]) for f in getattr(y, attr[0]).all() ))
         else:
-          # and this too
           x_attr_set = set(getattr(x, attr).all())
           y_attr_set = set(getattr(y, attr).all())
 
         score += len(x_attr_set & y_attr_set)
-        # print('score: {} {} {}'.format(x, score, x_attr_set & y_attr_set))
-
       return score
--- recommander/views.py
+++ recommander/views.py
@@ -3,28 +3,25 @@ from django.views.generic import TemplateView
 
 from account.models import Profile
 from .models import Activity
+from .engine import PredictionEngine
 import random
+
 class SuggestView(TemplateView):
     template_name = 'recommander/suggestions.html'
 
     def get(self, request, *args, **kwargs):
-      # collect data; maybe in PredictionPredictionEngine?
       population = self._collect_data(self.request.user)
-      # now instantce the engine for fitness computation over the sampling set
-      from .engine import PredictionEngine
       attrs=[('contacts', 'to'), ('skill_set', 'data'), 'projectpages']
+
       engine = PredictionEngine(self.request.user.profile, population, attrs=attrs)
+
       self.profiles_suggested = [ profile for profile in engine.predict() ]
-      print('suggested:', self.profiles_suggested)
       return super(SuggestView, self).get(request, *args, **kwargs)
 
     def _collect_data(self, user):
       user_profile = user.profile
 
-      activities_profiles_visited = user_profile.activities.values_list('profile', flat=True)
-      visited_profiles = Profile.objects.filter(pk__in=set(activities_profiles_visited))
 
-      # print("visited = {}".format([s.user for s in visited_profiles]))
       def random_choice(qs):
         try:
           choice = random.choice(qs)
@@ -32,26 +29,29 @@ class SuggestView(TemplateView):
           choice = None
         return choice
 
+      # crawling profile data
+      activities_profiles_visited = user_profile.activities.values_list('profile', flat=True)
+      visited_profiles = Profile.objects.filter(pk__in=set(activities_profiles_visited))
+
       a_skill = random_choice(user_profile.skill_set.all())
-      # print("skill selected {}".format(a_skill.data.codename))
-      if a_skill:
-        a_skill = a_skill.data
+      if a_skill: a_skill = a_skill.data
       skill_profiles = Profile.objects.filter(skill__data=a_skill).exclude(user=user)
-      # print('skill profiles ={}'.format([ s.user for s in skill_profiles]))
 
       a_project = random_choice(user_profile.projectpages.all())
-      project_profiles = Profile.objects.filter(projectpages=a_project).exclude(user=user)
-      # print('project profiles ={}'.format([ s.user for s in project_profiles]))
+      project_profiles = Profile.objects.filter(
+        projectpages=a_project, projectpages__isnull=False).exclude(user=user)
 
-      population_qs = visited_profiles.union(skill_profiles, project_profiles, visited_profiles)
+      # merging
+      population_qs = Profile.objects.none().union(
+        skill_profiles, project_profiles, visited_profiles
+        )
+      # filtering friends
       population_qs = population_qs.difference(Profile.objects.filter(contacts__to=user_profile))
-      # print(population_qs)
-      print('population: ', [ p.user.email for p in population_qs ])
+
       return population_qs
 
     def get_context_data(self, **kwargs):
         kwargs.update({
             'profiles_suggested': self.profiles_suggested
             })
-        print(kwargs['profiles_suggested'])
         return super(SuggestView, self).get_context_data(**kwargs)    
