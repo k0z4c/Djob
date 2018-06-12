bd5efe065fcf3ab109e8cbd1f1c8d6a208538db6
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-12 18:19:51 +0200

    Fix add_member_to_page

--- projectpager/signals.py
+++ projectpager/signals.py
@@ -1,17 +1,29 @@
 from django.dispatch import receiver
 from marathon.models import SocialRequest
+from django.db.models.signals import post_save
 from marathon.signals import social_request_accepted
+from django.core import serializers
+from .models import ProjectPage
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 @receiver(social_request_accepted, sender=SocialRequest)
 def add_member_to_page(sender, instance, **kwargs):
   if instance.label == 'invite_request':
-    from django.contrib.auth.models import Permission
-    from django.contrib.contenttypes.models import ContentType
-    from .models import ProjectPage
-
     content_type = ContentType.objects.get_for_model(ProjectPage)
     permission = Permission.objects.get(
       content_type=content_type,
       codename='can_post_discussions'
       )
+    projectpage = list(
+      serializers.deserialize('json', instance.data['project'])
+    )[0].object
+    projectpage.refresh_from_db()
+    projectpage.profiles.add(instance.to)
     instance.to.user.user_permissions.add(permission)
+
+@receiver(post_save, sender=ProjectPage)
+def add_owner_to_profiles(sender, instance, created, **kwargs):
+  if created:
+    owner = instance.owner
+    instance.profiles.add(owner)
