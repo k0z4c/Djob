e61785c3ed43bd0f229ffa83bbea29f522525c54
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-13 02:30:23 +0200

    Minor

--- projectpager/signals.py
+++ projectpager/signals.py
@@ -1,15 +1,10 @@
+from .models import ProjectPage
+from django.core import serializers
 from django.dispatch import receiver
 from marathon.models import SocialRequest
-from django.db.models.signals import post_save
 from marathon.signals import social_request_accepted
-from django.core import serializers
-from .models import ProjectPage
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
-
-
-from guardian.models import UserObjectPermission
 from guardian.shortcuts import assign_perm
+from django.db.models.signals import post_save
 
 @receiver(social_request_accepted, sender=SocialRequest)
 def add_member_to_page(sender, instance, **kwargs):
@@ -17,7 +12,7 @@ def add_member_to_page(sender, instance, **kwargs):
     projectpage = list(
       serializers.deserialize('json', instance.data['project'])
     )[0].object
-    
+
     projectpage.refresh_from_db()
     projectpage.profiles.add(instance.to)
     assign_perm('can_open_threads', instance.to.user, projectpage)
