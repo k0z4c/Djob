9473c26ab85fa12dd8f7bb47148b344bf6c73809
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-14 17:19:24 +0200

    Refactor

--- static/js/ajax_menu.js
+++ static/js/ajax_menu.js
@@ -1,81 +1,25 @@
-// function fetchFriendship(){
-//     $.ajax({
-//       url:  $('#friendship-badge').data('url'),
-//       error: function(jqXHR, textStatus, errorTrown){
-//         console.log(textStatus);
-//         console.log(errorTrown);
-//       },
-//       datatype: 'json',
-//       success: function(data){
-//         $('#friendship-badge').html(data.unread_notifications);
-//         if(data.unread_notifications > 0){
-//           $('#friendship-badge').html(data.unread_notifications);
-//           $('#friendship-badge').css('visibility', 'visible');
-//         }
-//         setTimeout(fetchFriendship,1000);
-//       }
-//     });
-//   }
+function FetchUnreadedNotificationsCount(target, key){
+  $.ajax({
+    url: target.data('url'),
+    datatype: 'json',
+    error: function(jqXHR, textStatus, errorTrown){
+             console.log(textStatus);
+             console.log(errorTrown);
+           },
+    success: function(data){
+      if(data[key] !== 0){
+        target.html(data[key]);
+        target.css('visibility', 'visible');
+      }
+      else{
+        target.css('visibility', 'hidden');
+      }
+    }
+  });
+  setTimeout(FetchUnreadedNotificationsCount, 1500, target, key);
+}
 
-// function fetchFriendshipList(){
-//   $.ajax({
-//     // url = {% url messanger:ajax_unreaded_count %}
-//     url: $('#friendship-request-list').data('url'),
-//     datatype: 'json',
-//     success: function(data){
-//       notifications = '';
-//       for (let k of Object.keys(data)){
-//         notifications += '<li class="px-auto dropdown-item">' + 
-//           `<img class="img-thumbnail rounded img-fluid" src=${data[k][3]}/>` + 
-//           `<a href=\"${data[k][2]}\"> ${data[k][0]} | ${formatDate(data[k][1])} </a>` +
-//           '</li>';
-//       }
-//       $('#friendship-request-list').html(notifications);
-//       setTimeout(fetchFriendshipList, 1000);
-//     }
-//   });
-// }
-
-// function fetchFriendshipList(){
-//   $.ajax({
-//     url: $('#friendship-request-list').data('url'),
-//     datatype: 'json',
-//     success: function(data){
-//       data.unread_list.forEach(function(entry){
-//         $('#friendship-request-list').append(`<a class="dropdown-item">${entry.description}</a>`);
-//       });
-//     }
-//   });
-// }
-
-// function fetchUnreadedMessagesCount(){
-//     $.ajax({
-//       url:  $('#messanger-badge').data('url'),
-//       error: function(jqXHR, textStatus, errorTrown){
-//         console.log(textStatus);
-//         console.log(errorTrown);
-//       },
-//       datatype: 'json',
-//       success: function(data){
-//         if(data.unreaded_count > 0){
-//           $('#messanger-badge').html(data.unreaded_count);
-//           $('#messanger-badge').css('visibility', 'visible');
-//         }
-//         setTimeout(fetchUnreadedMessagesCount,1000);
-//       }
-//     });
-//   }
-
-
-// function formatDate(date){
-//   var d = new Date(date);
-//   return d.getFullYear() + '/' + d.getMonth() + '/' + d.getDay()
-// }
-
-// $().ready(function(){
-//   $('#friendship-dropdown').click(fetchFriendshipList);
-// });
-// $(document).ready(function(){setTimeout(fetchFriendship, 1000);});
-// $(document).ready(function(){setTimeout(fetchFriendshipList, 1000);});
-// $(document).ready(function(){setTimeout(fetchUnreadedMessagesCount, 1000);});
-
+$().ready(function(){
+  setTimeout(FetchUnreadedNotificationsCount, 1500, $('#notifications-badge'), 'unread_count');
+  setTimeout(FetchUnreadedNotificationsCount, 1500, $('#messanger-badge'), 'unreaded_count');
+});
