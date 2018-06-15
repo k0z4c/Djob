a8a95d736d06bac8e31f152beeff0f49aa5f5de9
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-16 00:13:35 +0200

    Fix javascript bug

--- messanger/static/messanger/js/main.js
+++ messanger/static/messanger/js/main.js
@@ -1,15 +1,15 @@
 function localizeDatesJSON(sel, key){ 
-                sel.each(function(index, element){
-                    var obj;
-                    var date;
-                    obj = JSON.parse($(this).html());
-                    if(obj[key] != undefined){
-                        date = new Date(obj[key]);
-                        $(this).html(date.toLocaleString());
-                    } else {
-                        sel.html('not readed yet')
-                    }
-                    $(this).css('visibility', 'visible');
-                    return;
-                });
-            }
+  sel.each(function(index, element){
+      var obj;
+      var date;
+      obj = JSON.parse($(element).html());
+      if(obj[key] != undefined){
+          date = new Date(obj[key]);
+          $(element).html(date.toLocaleString());
+      } else {
+          $(this).html('not readed yet')
+      }
+      $(element).css('visibility', 'visible');
+      return;
+  });
+}
