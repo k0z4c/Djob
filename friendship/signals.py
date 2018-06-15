b174fab76735e2c52ae2a70ea8eeb1ec3ace5e16
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-15 17:45:17 +0200

    Add notification back for friendship request accepted.

--- friendship/signals.py
+++ friendship/signals.py
@@ -4,6 +4,7 @@ from .models import Friendship
 from marathon.models import SocialRequest
 from django.db import IntegrityError
 from django.db.models.signals import post_delete
+from notifications.signals import notify
 
 @receiver(social_request_accepted, sender=SocialRequest)
 def add_friend(sender, instance, **kwargs):
@@ -14,8 +15,13 @@ def add_friend(sender, instance, **kwargs):
         )
     except IntegrityError:
       pass
+    notify.send(
+      sender=instance.to.user,
+      recipient=instance.by.user,
+      verb='friendship_is_accepted_by',
+      description='{} has accepted you friendship request!'.format(instance.to.user)
+    )
 
 @receiver(post_delete, sender=Friendship)
 def remove_friendship(sender, instance, using, **kwargs):
-  print('friendship removed')
   Friendship.objects.filter(by=instance.to, to=instance.by).delete()
