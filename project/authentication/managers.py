b5d4d4d5519d4b22b932c7ba84a40caa6d672e10
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-17 16:32:41 +0200

    Fix authentication create_user

--- project/authentication/managers.py
+++ project/authentication/managers.py
@@ -8,7 +8,8 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Users must have an email address')
 
         user = self.model(
-            email=self.normalize_email(email)
+            email=self.normalize_email(email),
+            **extra_fields,
             )
         user.set_password(password)
         user.save(using=self._db)
