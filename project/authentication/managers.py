f47d8c1ec8b6667548603d02001913d38e1acf2e
Matteo Sondrio <matteo.sondrio@gmail.com>
2018-06-18 02:39:09 +0200

    Fix authentication superuser creation

--- project/authentication/managers.py
+++ project/authentication/managers.py
@@ -3,23 +3,32 @@ from django.contrib.auth.base_user import BaseUserManager
 # https://docs.djangoproject.com/en/2.0/topics/auth/customizing/#custom-users-and-permissions
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
         if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            **extra_fields,
-            )
+            raise ValueError('An email address must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+
     def create_superuser(self, email, password, **extra_fields):
-        user = create_user(
-            email,
-            password=password
-            )
-        user.is_admin = True
-        user.save(db=self._db)
-        return user
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
